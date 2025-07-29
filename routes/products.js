5ad64552933f966c295ec9bb939db998d10f5df3
ibtihelbs <ibtihel.bensalah@outlook.fr>
2025-07-29 13:30:10 +0100

    data

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express().router;
-
-router.get("/", (req, res) => {
-  res.send("ahla wsahla fi products");
+const products = require("../modal/products");
+router.get("/", async (req, res) => {
+  try {
+    const prod = await products.find();
+    res.status(200).send(prod);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
