820837711895fd40245cd2eede0f14987f16182f
ibtihelbs <ibtihel.bensalah@outlook.fr>
2025-07-29 12:56:05 +0100

    connect to mongodb

---
+++ main.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const PORT = 5000;
+const mongoose = require("mongoose");
+const app = express(express.json());
+const productsRoutes = require("./routes/products");
+mongoose
+  .connect(process.env.URL)
+  .then(console.log("database connected"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("hi the route is working ! !");
+});
+app.use("/products", productsRoutes);
+
+app.listen(PORT, () => {
+  console.log("server is running on port : " + PORT);
+});
